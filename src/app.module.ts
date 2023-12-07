df6564a5de2f3e1c75d96d5c4cbec3d6298c6160
Ahmed Raef <ahmedraef806@gmail.com>
2023-12-07 13:23:19 +0200

    remove un-used import and some formating

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
